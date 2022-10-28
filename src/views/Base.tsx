38f2dc1b4214c855cde37131204ed77b3911742b
Alexander <mrreads@yandex.com>
2022-10-28 22:50:41 +0300

    statistic card

--- src/views/Base.tsx
+++ src/views/Base.tsx
@@ -1,11 +1,15 @@
+import StatisticCard from "@/components/StatisticCard";
+
 export default function Base() {
     return (
         <div className="base">
-            <h1 className="title"> База </h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Commodi esse molestias necessitatibus aut adipisci illo nesciunt dolor corrupti quibusdam quasi saepe fugiat sed exercitationem, 
-                quam repellat. Vero, fuga? A, eveniet.
-            </p>
+            <h1 className="title"> Добро пожаловать! </h1>
+
+            <div className="base__statistic-card">
+                <StatisticCard number={143.233} icon='coin.svg' text="Ваш баланс" wallet={true} />
+                <StatisticCard number={7} icon='client.svg' text="Ваши клиенты" />
+            </div>
+
         </div>
     )
 }
