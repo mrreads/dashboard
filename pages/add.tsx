aac999f1af2eaab1b6d0c6b0d3197047fddd1555
Alexander <mrreads@yandex.com>
2022-12-20 21:26:20 +0300

    locale datepicker

--- pages/add.tsx
+++ pages/add.tsx
@@ -2,10 +2,12 @@ import { Formik, FormikHelpers, FormikProps, Form, Field, FieldProps } from 'for
 import { object, string, number, date } from 'yup';
 import { faker } from '@faker-js/faker';
 
-import DatePicker from "react-datepicker";
+import DatePicker, { registerLocale } from "react-datepicker";
+import ru from "date-fns/locale/ru";
 
 import { IClient } from '@/interfaces/IClient.type';
 
+registerLocale("ru", ru);
 export default function Add(): JSX.Element {
 
     const phoneRegExp = /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
@@ -42,46 +44,47 @@ export default function Add(): JSX.Element {
          <Form className='flex flex-col max-w-xs'>
 
             <div className='flex gap-4 justify-between'>
-                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ФИО:</label> 
+                <label htmlFor="name" className="custom-label">ФИО:</label> 
                 { errors.name && touched.name ? (<span className="text-sm text-red-600 dark:text-red-500">{errors.name}</span>) : null }
             </div>
-           <Field id="name" name="name" placeholder="ФИО" className="mb-5 bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+           <Field id="name" name="name" placeholder="ФИО" className="mb-5 custom-input" />
 
            <div className='flex gap-4 justify-between'>
-                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Почта:</label> 
+                <label htmlFor="name" className="custom-label">Почта:</label> 
                 { errors.email && touched.email ? (<span className="text-sm text-red-600 dark:text-red-500">{errors.email}</span>) : null }
             </div>
-           <Field id="email" name="email" placeholder="Почта"  className="mb-5 bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+           <Field id="email" name="email" placeholder="Почта"  className="mb-5 custom-input" />
 
            <div className='flex gap-4 justify-between'>
-                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Изображение: <span className='opacity-70'> (генерируется автоматически) </span> </label> 
+                <label htmlFor="name" className="custom-label"> Изображение: <span className='opacity-70'> (генерируется автоматически) </span> </label> 
             </div>
-           <Field id="image" name="image" placeholder="Изображение сгенерируется автоматически" disabled className="opacity-30 cursor-not-allowed mb-5 bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+           <Field id="image" name="image" placeholder="Изображение сгенерируется автоматически" disabled className="mb-5 custom-input cursor-no-drop opacity-50" />
 
            <div className='flex gap-4 justify-between'>
-                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Телефон:</label> 
+                <label htmlFor="name" className="custom-label">Телефон:</label> 
                 { errors.phone && touched.phone ? (<span className="text-sm text-red-600 dark:text-red-500">{errors.phone}</span>) : null }
             </div>
-           <Field id="phone" name="phone" placeholder="Телефон"  className="mb-5 bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+           <Field id="phone" name="phone" placeholder="Телефон"  className="mb-5 custom-input" />
 
            <div className='flex gap-4 justify-between'>
-                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Дата рождения:</label> 
+                <label htmlFor="name" className="custom-label">Дата рождения:</label> 
                 { errors.birth && touched.birth ? (<span className="text-sm text-red-600 dark:text-red-500">{errors.birth}</span>) : null }
             </div>
-           <Field id="birth" name="birth" type="date" placeholder="Дата рождения">
+           <Field id="birth" name="birth">
                 {({ field, form: { setFieldValue } }) => (
                     <DatePicker
+                        locale="ru" dateFormat="dd.MM.yyyy"
                         popperClassName="react-datepicker-left" nextMonthButtonLabel=">" previousMonthButtonLabel="<"
-                        className="mb-5 bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="mb-5 custom-input" placeholderText={'ДД/ММ/ГГ'}
                         {...field} selected={field.value || null} onChange={(val: any) => setFieldValue(field.name, val)}
                 />)}
             </Field>
 
            <div className='flex gap-4 justify-between'>
-                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Компания:</label> 
+                <label htmlFor="name" className="custom-label">Компания:</label> 
                 { errors.company && touched.company ? (<span className="text-sm text-red-600 dark:text-red-500">{errors.company}</span>) : null }
             </div>
-           <Field id="phone" name="company" placeholder="Телефон"  className="mb-5 bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+           <Field id="phone" name="company" placeholder="Телефон"  className="mb-5 custom-input" />
            
            <button type="submit" className="w-auto bg-slate-500 text-slate-100 py-3 rounded-md hover:bg-slate-600 dark:bg-slate-800 dark:text-slate-300 dark:hover:bg-slate-700">Создать</button>
          
